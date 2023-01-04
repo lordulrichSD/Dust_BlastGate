b5431fb79256648e52c4c56e0d48af6106c266b5
lordulrichSD <adam.wellner@gmail.com>
2023-01-03 19:45:08 -0600

    Created Firmware folder

---
+++ Firmware/Settings.py
@@ -0,0 +1,7 @@
+#configuration settings - i.e. wifi Deployed Device Specific
+SSID = 'examplessid'
+password = 'examplepassword'
+DCIP='192.168.1.1'
+closedPos=75
+openPos=25
+delayTime=1
--- Settings.py
+++
@@ -1,7 +0,0 @@
-#configuration settings - i.e. wifi Deployed Device Specific
-SSID = 'examplessid'
-password = 'examplepassword'
-DCIP='192.168.1.1'
-closedPos=75
-openPos=25
-delayTime=1
